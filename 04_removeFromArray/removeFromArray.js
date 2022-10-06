f1fc57d8ed25f6a2bd7ee9d7ebfdb2427ae642ae
Arch7339 <raymond17399@gmail.com>
2022-10-06 15:44:38 +0530

    Fix js-ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+	let newArray = [...array];
+	for (let i=0; i<array.length; i++){
+		if(newArray.includes(remove[i])){
+		newArray.splice(newArray.indexOf(remove[i]),1);		
+		}
+	}
+	return newArray;
 };
+removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
