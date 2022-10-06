45a74af02bdab7aae042b12897de8f4da1225828
Arch7339 <raymond17399@gmail.com>
2022-10-06 12:22:33 +0530

    Fix js-ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	return [...str].reverse().join('');
 };
-
+reverseString("");
 // Do not edit below this line
 module.exports = reverseString;
