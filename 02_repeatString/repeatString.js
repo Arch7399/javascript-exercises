f0c85d9c9110bea638646a6042309ae691376df0
Arch7339 <raymond17399@gmail.com>
2022-10-06 12:17:01 +0530

    Fix js-ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(letter, num) {
+	let word = '';
+	if (num<0){
+	word = "ERROR";}
+	else {
+	for (let i=0;i<num;i++){
+		word += letter;}}
+	return word;
 };
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
