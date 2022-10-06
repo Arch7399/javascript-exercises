8f0d3d358c5d9c8a58002302ba0a2fd1b15b4f32
Arch7339 <raymond17399@gmail.com>
2022-10-06 20:30:46 +0530

    Fix js-ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function (x, y) {
+if (x > 0 && y > 0 && typeof x === 'number' && typeof y === 'number') {
+    var total = 0;
+    if (x < y) {
+        for (var i = x; i <= y; i++) {
+            total += i;
+        }
+        return total;
+    } else if (x > y) {
+        for (var i = y; i <= x; i++) {
+            total += i;
+        }
+        return total;
+    }
+} else {
+    return 'ERROR';
+ }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
