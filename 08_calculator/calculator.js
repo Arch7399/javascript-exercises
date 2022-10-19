0408e92e4d0b65e96073dbba276cf0cefe9197e9
Arch7339 <raymond17399@gmail.com>
2022-10-19 19:28:49 +0530

    js-ex08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summo = 0;
+  array.forEach(element => {
+    summo += element;
+  });
+  return summo;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  arr.forEach(element => {
+    prod *= element;
+  });
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  let raised = 1;
+	for(i=0; i<pow; i++){
+    raised *= num;
+  }
+  return raised;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fac = 1;
+  for(i=0; i<num; i++){
+    fac *= (num - i);
+  }
+  return fac;
 };
+
+add(0, 0);
+subtract(6, 4);
+sum([7]);
+multiply([2]);
+power(4, 3);
+factorial(0);
 
 // Do not edit below this line
 module.exports = {
