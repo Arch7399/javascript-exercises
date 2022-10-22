d4b57a13246edb62838a24c8860f90476a1db421
Arch7339 <raymond17399@gmail.com>
2022-10-22 09:59:08 +0530

    js ex-09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+   let str = word.replace(/[^a-zA-Z]/gi, '').toLowerCase();
+   let neww = str.split('').reverse().join('');
+   return neww === str ? true: false;
 };
+
+
+palindromes('racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
