b3f29270918371aef5f66a08f4028c645df4e4c8
Arch7339 <raymond17399@gmail.com>
2022-10-22 14:16:14 +0530

    js ex-12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
 
+
+const findTheOldest = function(ppl) {
+    let aLived, bLived;
+    const lived = ppl.sort((a, b) => {
+        if(!a.hasOwnProperty('yearOfDeath')){
+            aLived = new Date().getFullYear() - a.yearOfBirth;
+        } else {
+        aLived = a.yearOfDeath - a.yearOfBirth;}
+        if(!b.hasOwnProperty('yearOfDeath')){
+            bLived = new Date().getFullYear() - b.yearOfBirth;
+        } else {
+        bLived = b.yearOfDeath - b.yearOfBirth;}
+
+        return aLived > bLived ? -1: 1;
+    })
+    return lived[0];
 };
-
+findTheOldest([]);
 // Do not edit below this line
 module.exports = findTheOldest;
