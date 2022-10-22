a954db57011846acd929e00af183e47024638c49
Arch7339 <raymond17399@gmail.com>
2022-10-22 11:48:45 +0530

    js ex-11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const arr = array.map(book => book.title);
+    return arr;
 };
+
+getTheTitles([]);
 
 // Do not edit below this line
 module.exports = getTheTitles;
